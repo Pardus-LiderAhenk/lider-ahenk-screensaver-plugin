3353e3ed7586a25dbf3c332a234052f2c5e49a06
mine.dogan <mine.dogan@agem.com.tr>
2016-06-15 17:14:09 +0300

    revision

--- ahenk-screensaver/screensaver/policy.py
+++ ahenk-screensaver/screensaver/policy.py
@@ -4,13 +4,14 @@
 
 import json
 
-from base.plugin.AbstractCommand import AbstractCommand
+from base.plugin.abstract_plugin import AbstractPlugin
 
-class Screensaver(AbstractCommand):
+class Screensaver(AbstractPlugin):
     def __init__(self, data, context):
         super(Screensaver, self).__init__()
         self.data = data
         self.context = context
+        self.logger = self.get_logger()
 
         self.context.put('message_type', 'qwe')
         self.context.put('message_code', 'qwe')
@@ -28,49 +29,45 @@ class Screensaver(AbstractCommand):
 
                 data = json.loads(self.data)
 
-                xscreensaver_file.write('mode: ' + str(data['mode']) + '\n')
-                xscreensaver_file.write('timeout: ' + str(data['timeout']) + '\n')
-                xscreensaver_file.write('cycle: ' + str(data['cycle']) + '\n')
-                xscreensaver_file.write('lock: ' + str(data['lock']) + '\n')
-                xscreensaver_file.write('lockTimeout: ' + str(data['lockTimeout']) + '\n')
-                xscreensaver_file.write('grabDesktopImages: ' + str(data['grabDesktopImages']) + '\n')
-                xscreensaver_file.write('grabVideoFrames: ' + str(data['grabVideoFrames']) + '\n')
-                xscreensaver_file.write('dpmsEnabled: ' + str(data['dpmsEnabled']) + '\n')
-                xscreensaver_file.write('dpmsStandby: ' + str(data['dpmsStandby']) + '\n')
-                xscreensaver_file.write('dpmsSuspend: ' + str(data['dpmsSuspend']) + '\n')
-                xscreensaver_file.write('dpmsOff: ' + str(data['dpmsOff']) + '\n')
-                xscreensaver_file.write('dpmsQuickOff: ' + str(data['dpmsQuickOff']) + '\n')
-                xscreensaver_file.write('date: ' + str(data['date']) + '\n')
-                xscreensaver_file.write('literal: ' + str(data['literal']) + '\n')
-                xscreensaver_file.write('textLiteral: ' + str(data['textLiteral']) + '\n')
-                xscreensaver_file.write('url: ' + str(data['url']) + '\n')
-                xscreensaver_file.write('textUrl: ' + str(data['textUrl']) + '\n')
-                xscreensaver_file.write('fade: ' + str(data['fade']) + '\n')
-                xscreensaver_file.write('unfade: ' + str(data['unfade']) + '\n')
-                xscreensaver_file.write('fadeSeconds: ' + str(data['fadeSeconds']) + '\n')
-                xscreensaver_file.write('installColormap: ' + str(data['installColormap']) + '\n')
-
+                content = 'mode: ' + str(data['mode']) + '\n' \
+                            'timeout: ' + str(data['timeout']) + '\n' \
+                            'cycle: ' + str(data['cycle']) + '\n' \
+                            'lock: ' + str(data['lock']) + '\n' \
+                            'lockTimeout: ' + str(data['lockTimeout']) + '\n' \
+                            'grabDesktopImages: ' + str(data['grabDesktopImages']) + '\n' \
+                            'grabVideoFrames: ' + str(data['grabVideoFrames']) + '\n' \
+                            'dpmsEnabled: ' + str(data['dpmsEnabled']) + '\n' \
+                            'dpmsStandby: ' + str(data['dpmsStandby']) + '\n' \
+                            'dpmsSuspend: ' + str(data['dpmsSuspend']) + '\n' \
+                            'dpmsOff: ' + str(data['dpmsOff']) + '\n' \
+                            'dpmsQuickOff: ' + str(data['dpmsQuickOff']) + '\n' \
+                            'date: ' + str(data['date']) + '\n' \
+                            'literal: ' + str(data['literal']) + '\n' \
+                            'textLiteral: ' + str(data['textLiteral']) + '\n' \
+                            'url: ' + str(data['url']) + '\n' \
+                            'textUrl: ' + str(data['textUrl']) + '\n' \
+                            'fade: ' + str(data['fade']) + '\n' \
+                            'unfade: ' + str(data['unfade']) + '\n' \
+                            'fadeSeconds: ' + str(data['fadeSeconds']) + '\n' \
+                            'installColormap: ' + str(data['installColormap']) + '\n'
+                
+                xscreensaver_file.write(content)
                 xscreensaver_file.close()
 
-                self.set_result('POLICY_STATUS', 'POLICY_PROCESSED', 'User screensaver profile processed successfully.')
+                self.logger.debug('[Screensaver] Creating response.')
+                self.context.create_response(code=self.get_message_code().POLICY_PROCESSED.value,
+                                             message='Screensaver policy executed successfully')
                 self.logger.info('[Screensaver] Screensaver profile is handled successfully.')
 
             else:
-                self.set_result('POLICY_STATUS', 'POLICY_PROCESSED', 'There is no username.')
+                raise Exception('There is no username.')
+
         except Exception as e:
-            self.logger.error('[Screensaver] A problem occured while handling screensaver profile: {0}'.format(str(e)))
-            self.set_result('POLICY_STATUS', 'POLICY_PROCESSED',
-                            'A problem occured while handling screensaver profile: {0}'.format(str(e)))
+            self.logger.error('[Screensaver] A problem occurred while handling Screensaver policy. Error Message: {}'.format(str(e)))
+            self.context.create_response(code=self.get_message_code().POLICY_ERROR.value,
+                                         message='A problem occurred while handling Screensaver policy')
 
-
-    def set_result(self, type=None, code=None, message=None, data=None, content_type=None):
-        self.context.put('message_type', type)
-        self.context.put('message_code', code)
-        self.context.put('message', message)
-        # self.context.put('data')
-        # self.context.put('content_type')
 
 def handle_policy(profile_data, context):
-    print('SCREENSAVER')
-    quota = Screensaver(profile_data, context)
-    quota.handle_policy()
+    screensaver = Screensaver(profile_data, context)
+    screensaver.handle_policy()
