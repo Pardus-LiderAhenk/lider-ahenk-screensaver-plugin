8c7dcb23989d9bdfd9336623b5b4717dd4932766
Volkan Şahin <bm.volkansahin@gmail.com>
2017-03-07 16:50:49 +0300

    screensaver plugin available for machine profiles now

--- ahenk-screensaver/screensaver/policy.py
+++ ahenk-screensaver/screensaver/policy.py
@@ -15,69 +15,61 @@ class Screensaver(AbstractPlugin):
         self.logger = self.get_logger()
         self.message_code = self.get_message_code()
 
-        self.context.put('message_type', 'qwe')
-        self.context.put('message_code', 'qwe')
-        self.context.put('message', 'qwe')
-        self.context.put('data', None)
-        self.context.put('content_type', None)
-
         self.logger.debug('Parameters were initialized.')
 
+    def user_xscreen_conf_path(self, username):
+        self.logger.debug('u1')
+        user_home_path = self.execute('echo $( getent passwd "{0}" | cut -d: -f6 )'.format(username))[1]
+        return '{0}/.xscreensaver'.format(user_home_path.strip())
+
+    def create_content(self):
+        data = json.loads(self.data)
+        self.logger.debug('c1')
+        return 'mode: {0}\ntimeout: {1}\ncycle: {2}\nlock: {3}\nlockTimeout: {4}\ngrabDesktopImages: {5}\n' \
+               'grabVideoFrames: {6}\ndpmsEnabled: {7}\ndpmsStandby: {8}\ndpmsSuspend: {9}\ndpmsOff: {10}' \
+               '\ndpmsQuickOff: {11}\ntextMode: {12}\ntextLiteral: {13}\ntextUrl: {14}\nfade: {15}\nunfade:' \
+               ' {16}\nfadeSeconds: {17}\ninstallColormap: {18}\n'.format(data['mode'], data['timeout'],
+                                                                           data['cycle'], data['lock'],
+                                                                           data['lockTimeout'],
+                                                                           data['grabDesktopImages'],
+                                                                           data['grabVideoFrames'],
+                                                                           data['dpmsEnabled'],
+                                                                           data['dpmsStandby'],
+                                                                           data['dpmsSuspend'],
+                                                                           data['dpmsOff'],
+                                                                           data['dpmsQuickOff'],
+                                                                           data['textMode'],
+                                                                           data['textLiteral'],
+                                                                           data['textUrl'], data['fade'],
+                                                                           data['unfade'],
+                                                                           data['fadeSeconds'],
+                                                                           data['installColormap'])
+
     def handle_policy(self):
-        username = self.context.get('username')
 
         try:
+            username = self.context.get('username')
+            content = self.create_content()
+
             if username is not None:
-                xfile_path = '/home/' + str(username) + '/.xscreensaver'
-                xscreensaver_file = open(xfile_path, 'w+')
+                xfile_path = self.user_xscreen_conf_path(username)
+                self.delete_file(xfile_path)
+                self.write_file(xfile_path, content, 'w+')
 
-                data = json.loads(self.data)
-
-                content = 'mode: ' + str(data['mode']) + '\n' \
-                                                         'timeout: ' + str(data['timeout']) + '\n' \
-                                                                                              'cycle: ' + str(
-                    data['cycle']) + '\n' \
-                                     'lock: ' + str(data['lock']) + '\n' \
-                                                                    'lockTimeout: ' + str(data['lockTimeout']) + '\n' \
-                                                                                                                 'grabDesktopImages: ' + str(
-                    data['grabDesktopImages']) + '\n' \
-                                                 'grabVideoFrames: ' + str(data['grabVideoFrames']) + '\n' \
-                                                                                                      'dpmsEnabled: ' + str(
-                    data['dpmsEnabled']) + '\n' \
-                                           'dpmsStandby: ' + str(data['dpmsStandby']) + '\n' \
-                                                                                        'dpmsSuspend: ' + str(
-                    data['dpmsSuspend']) + '\n' \
-                                           'dpmsOff: ' + str(data['dpmsOff']) + '\n' \
-                                                                                'dpmsQuickOff: ' + str(
-                    data['dpmsQuickOff']) + '\n' \
-                                            'textMode: ' + str(data['textMode']) + '\n' \
-                                                                                   'textLiteral: ' + str(
-                    data['textLiteral']) + '\n' \
-                                           'textUrl: ' + str(data['textUrl']) + '\n' \
-                                                                                'fade: ' + str(data['fade']) + '\n' \
-                                                                                                               'unfade: ' + str(
-                    data['unfade']) + '\n' \
-                                      'fadeSeconds: ' + str(data['fadeSeconds']) + '\n' \
-                                                                                   'installColormap: ' + str(
-                    data['installColormap']) + '\n'
-
-                xscreensaver_file.write(content)
-                xscreensaver_file.close()
-
-                self.logger.debug('Config file content: {}'.format(content))
-
-                change_owner = 'chown ' + username + ':' + username + ' ' + xfile_path
-                self.execute(change_owner)
+                self.logger.debug('Config file content: \n{0}'.format(content))
+                self.execute('chown {0}:{0} {1}'.format(username, xfile_path))
                 self.logger.debug('.xscreensaver owner is changed.')
-
-                self.logger.info('Screensaver profile is handled successfully.')
-                self.context.create_response(code=self.message_code.POLICY_PROCESSED.value,
-                                             message='Kullanıcı screensaver profili başarıyla çalıştırıldı.')
+                self.logger.info('Screensaver profile is handled successfully for user.')
 
             else:
-                self.logger.error('Screensaver profile is user-oriented, not machine-oriented.')
-                self.context.create_response(code=self.message_code.POLICY_ERROR.value,
-                                             message='Screensaver profili sadece kullanıcı tabanlıdır.')
+                for user in self.Sessions.user_name():
+                    self.delete_file(self.user_xscreen_conf_path(user))
+                    self.write_file(self.user_xscreen_conf_path(user), content, 'w+')
+                # self.write_file('/etc/X11/app-defaults/XScreenSaver', content, 'w')
+                self.logger.error('Screensaver profile is handled successfully for machine.')
+            self.execute('xscreensaver-command -restart',as_user=self.context.get_username())
+            self.context.create_response(code=self.message_code.POLICY_PROCESSED.value,
+                                         message='Kullanıcı screensaver profili başarıyla çalıştırıldı.')
 
         except Exception as e:
             self.logger.error('A problem occured while handling screensaver profile: {0}'.format(str(e)))
